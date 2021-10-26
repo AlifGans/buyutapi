3374d0c1acf465bc73c0ec13342590442446267c
Alif Jian <alifjian834@gmail.com>
2021-10-26 20:07:31 +0700

    update api

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const textFancyGlow = require("./textpro/fancyGlow")
+const textPro = require("./textpro/textPro.js")
 const getInfo = require('./ytdl.js')
 const app = express();
 const port = process.env.PORT || 3030;
@@ -26,22 +26,55 @@ app.get('/ytmp4/',(req,res) => {
     }
     
 })
-// app.get('/ytmp4/',(req,res) => {
-//     
-// })
+
+// fancyglow textpro
 app.get('/textpro/fancyglow',(req,res) => {
-    const text = req.query.text;
+    const text = req.query.text0;
+    const url = "https://textpro.me/free-advanced-glow-text-effect-873.html"
     res.header("Content-Type",'application/json');
     if(Object.keys(req.query).length === 0) {
-        res.json({error : 'Example https://buyutapi.herokuapp.com/textpro/fancyglow?text=buyutapi'});
+        res.json({error : 'Example https://buyutapi.herokuapp.com/textpro/fancyglow?text0=buyutapi'});
     }
     if(text == ""){
         res.json({error : 'Fill text query'});
     }else{
-        textFancyGlow(text)
+        textPro(url,text)
         .then( url => res.json({url}))
     }
 })
+// Pornhub textpro
+app.get('/textpro/pornhub',(req,res) => {
+    const text = [req.query.text0,req.query.text1];
+    const url = "https://textpro.me/pornhub-style-logo-online-generator-free-977.html";
+
+    res.header("Content-Type",'application/json');
+    if(Object.keys(req.query).length === 0) {
+        res.json({error : 'Example https://buyutapi.herokuapp.com/textpro/pornhub?text0=buyut&text1=api'});
+    }
+    if(text.length == 0){
+        res.json({error : 'Example https://buyutapi.herokuapp.com/textpro/pornhub?text0=buyut&text1=api'});
+    }else{
+        textPro(url,text)
+        .then( url => res.json({url}))
+    }
+})
+// Glitch textpro
+app.get('/textpro/glitch',(req,res) => {
+    const text = [req.query.text0,req.query.text1];
+    const url = "https://textpro.me/create-glitch-text-effect-style-tik-tok-983.html";
+
+    res.header("Content-Type",'application/json');
+    if(Object.keys(req.query).length === 0) {
+        res.json({error : 'Example https://buyutapi.herokuapp.com/textpro/glitch?text0=buyut&text1=api'});
+    }
+    if(text.length == 0){
+        res.json({error : 'Example https://buyutapi.herokuapp.com/textpro/glitch?text0=buyut&text1=api'});
+    }else{
+        textPro(url,text)
+        .then( url => res.json({url}))
+    }
+})
+
 // app.get('/ytmp4/',(req,res) => {
 //     
 // })
---
+++ textpro/textPro.js
@@ -0,0 +1,35 @@
+const puppeteer = require("puppeteer");
+async function textPro(url,textInput){
+	let output;
+	const browser = await puppeteer.launch({
+			'args' : [
+    '--no-sandbox',
+    '--disable-setuid-sandbox'
+  ],
+			headless : false
+		});
+	const page = await browser.newPage();
+	await page
+			.goto(url,
+				{waitUntil : "networkidle2"})
+			.then( async () => {
+				await textInput.forEach( async (value,i) => {	
+					await page.type(`#text-${i}`, value);
+					console.log(`typing #text-${i} value : ${value}`)
+				})
+				await page.click("#submit");
+				await new Promise(resolve => setTimeout(resolve,20000));
+				const element = await page.$(
+					`div[class=thumbnail] > img`
+					);
+				const text = await ( await element.getProperty("src")).jsonValue();
+				output = text;
+				await browser.close();		
+			}).catch(err => {
+				return err;
+			})
+	return output;
+}
+
+module.exports = textPro;
+
