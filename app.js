e2be6f4f0be4fdf1a8422e772dac33146a383af3
Alif Jian <alifjian834@gmail.com>
2021-10-26 17:24:58 +0700

    Update API

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+const textFancyGlow = require("./textpro/fancyGlow")
 const getInfo = require('./ytdl.js')
 const app = express();
 const port = process.env.PORT || 3030;
@@ -18,6 +20,49 @@ app.get('/ytmp4/',(req,res) => {
         })
     }
     
+})
+// app.get('/ytmp4/',(req,res) => {
+//     
+// })
+app.listen(port,()=>{
+    console.log(`Server Running In http://localhost:${port}`);
+})
+const express = require('express');
+const getInfo = require('./ytdl.js')
+const app = express();
+const port = process.env.PORT || 3030;
+
+app.set('json spaces', 2);
+app.get("/", (req,ress) => {
+    res.send("YTMP4 API https://buyutapi.herokuapp.com/ytmp4?url=youtubelink")
+})
+app.get('/ytmp4/',(req,res) => {
+    const url = req.query.url;
+    res.header("Content-Type",'application/json');
+    if(Object.keys(req.query).length === 0) {
+        res.json({error : 'Cant identifying url (ytmp4?url=youtubeurl)'});
+    }
+    if(url == ""){
+        res.json({error : 'Fill url query'});
+    }else{
+        getInfo(url,info => {
+            res.send(info)
+        })
+    }
+    
+})
+app.get('/textpro/fancyglow',(req,res) => {
+    const text = req.query.text;
+    res.header("Content-Type",'application/json');
+    if(Object.keys(req.query).length === 0) {
+        res.json({error : 'Example https://buyutapi.herokuapp.com/textpro/fancyglow?text=buyutapi'});
+    }
+    if(url == ""){
+        res.json({error : 'Fill text query'});
+    }else{
+        textFancyGlow(text)
+        .then( url => res.json({url}))
+    }
 })
 // app.get('/ytmp4/',(req,res) => {
 //     
